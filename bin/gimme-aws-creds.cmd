d44d1cefaefeea25b046555c146fec12ebb6ff32
Thiago Bellotto <thiago.br@live.com>
2024-05-21 09:38:32 -0700

    Remove part that breaks the windows usage

--- bin/gimme-aws-creds.cmd
+++ bin/gimme-aws-creds.cmd
@@ -9,13 +9,7 @@ for %%i in (cmd bat exe) do (
         call :SetPythonExe "%%~$PATH:j"
     )
 )
-for /f "tokens=2 delims==" %%i in ('assoc .py') do (
-    for /f "tokens=2 delims==" %%j in ('ftype %%i') do (
-        for /f "tokens=1" %%k in ("%%j") do (
-            call :SetPythonExe %%k
-        )
-    )
-)
+
 for %%i in (cmd bat exe) do (
     for %%j in (python.%%i) do (
         call :SetPythonExe "%%~$PATH:j"
